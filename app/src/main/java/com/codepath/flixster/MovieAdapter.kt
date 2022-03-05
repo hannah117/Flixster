3ecdfea0c8b2a66b9b7d761f0b06f45dce041633
hannah117 <hannahismail711@gmail.com>
2022-03-05 01:13:26 -0600

    Added details screen with movie rating

--- app/src/main/java/com/codepath/flixster/MovieAdapter.kt
+++ app/src/main/java/com/codepath/flixster/MovieAdapter.kt
@@ -1,15 +1,18 @@
 package com.codepath.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private var TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -27,15 +30,27 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
     }
 
     override fun getItemCount() = movies.size
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
         private val tvPoster = itemView.findViewById<ImageView>(R.id.tvPoster)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             Log.i(TAG,movie.posterImageUrl)
             Glide.with(context).load(movie.posterImageUrl).into(tvPoster)
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 
